be58d9f52add43c53002495e62d1da6fb8bca0bc
joaovictorjs <skai7ami@gmail.com>
2023-03-07 17:16:33 -0300

    novos contrutores

--- lib/models/keyboard_item.dart
+++ lib/models/keyboard_item.dart
@@ -9,12 +9,25 @@ class KeyboardItem {
     required this.type,
     required this.text,
     required this.value,
-    required this.span,
+    this.span = 1,
   });
 
   KeyboardItem.numeric(this.value) {
     text = value;
     span = 1;
     type = KeyboardItemType.numeric;
+  }
+
+  KeyboardItem.value({
+    required this.value,
+    this.span = 1,
+    required this.type,
+  }) {
+    text = value;
+  }
+
+  KeyboardItem.basicOperation({required this.value, this.span = 1}) {
+    text = value;
+    type = KeyboardItemType.basicOperation;
   }
 }
