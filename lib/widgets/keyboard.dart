1424e6672f5f74994b22ca3da9e7c7243d15be8a
joaovictorjs <skai7ami@gmail.com>
2023-03-07 17:18:40 -0300

    botões do keyboard

--- lib/widgets/keyboard.dart
+++ lib/widgets/keyboard.dart
@@ -1,7 +1,11 @@
 import 'package:calculator/states/app_theme/app_theme_bloc.dart';
 import 'package:calculator/states/app_theme/app_theme_state.dart';
+import 'package:calculator/states/keyboard/keyboard_bloc.dart';
+import 'package:calculator/states/keyboard/keyboard_state.dart';
+import 'package:calculator/widgets/keyboard_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class Keyboard extends StatelessWidget {
   const Keyboard({super.key});
@@ -10,6 +14,7 @@ class Keyboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<AppThemeBloc, AppThemeState>(
       builder: (context, appTheme) => Container(
+        padding: const EdgeInsets.all(5),
         decoration: BoxDecoration(
           color: appTheme.theme.background,
           boxShadow: [
@@ -20,7 +25,26 @@ class Keyboard extends StatelessWidget {
             )
           ],
         ),
-        child: Center(),
+        child: LayoutBuilder(builder: (context, constraints) {
+          var height = (constraints.maxHeight - 20) / 5;
+          return BlocBuilder<KeyboardBloc, KeyboardState>(
+            builder: (context, state) {
+              return StaggeredGrid.count(
+                crossAxisCount: 4,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+                axisDirection: AxisDirection.down,
+                children: List.generate(
+                  (state as KeyboardReady).keys.length,
+                  (index) => KeyboardButton(
+                    item: state.keys[index],
+                    height: height,
+                  ),
+                ),
+              );
+            },
+          );
+        }),
       ),
     );
   }
---
+++ lib/widgets/keyboard_button.dart
@@ -0,0 +1,45 @@
+import 'package:calculator/models/keyboard_item.dart';
+import 'package:calculator/models/keyboard_item_type.dart';
+import 'package:calculator/states/app_theme/app_theme_bloc.dart';
+import 'package:calculator/states/keyboard/keyboard_bloc.dart';
+import 'package:calculator/states/keyboard/keyboard_event.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class KeyboardButton extends StatelessWidget {
+  KeyboardButton({super.key, required this.item, required this.height});
+  late KeyboardItem item;
+  late double height;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = BlocProvider.of<AppThemeBloc>(context).state.theme;
+    var shape = RoundedRectangleBorder(borderRadius: BorderRadius.circular(4));
+
+    return StaggeredGridTile.extent(
+        crossAxisCellCount: item.span,
+        mainAxisExtent: height,
+        child: Material(
+          shape: shape,
+          color: item.type == KeyboardItemType.numeric
+              ? Colors.transparent
+              : theme.button,
+          child: InkWell(
+            onTap: () => {
+              //if item is parentheses -> replaceParentheses item and updates display, else updates display
+            },
+            customBorder: shape,
+            hoverColor: theme.hover,
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
+            child: Center(
+              child: Text(
+                item.text,
+                style: TextStyle(color: theme.text, fontSize: 16),
+              ),
+            ),
+          ),
+        ));
+  }
+}
