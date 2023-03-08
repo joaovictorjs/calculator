5e85fd70fe840d5918fa1568600e619e26048aa1
joaovictorjs <skai7ami@gmail.com>
2023-03-08 09:40:10 -0300

    botoes do display

--- lib/widgets/display_bar.dart
+++ lib/widgets/display_bar.dart
@@ -1,24 +1,43 @@
+import 'package:calculator/states/app_theme/app_theme_bloc.dart';
+import 'package:calculator/states/app_theme/app_theme_event.dart';
+import 'package:calculator/states/app_theme/app_theme_state.dart';
 import 'package:calculator/widgets/display_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DisplayBar extends StatelessWidget {
   const DisplayBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var appTheme = BlocProvider.of<AppThemeBloc>(context);
+
     return SizedBox(
       height: 40,
       width: MediaQuery.of(context).size.width,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          DisplayButton(iconData: Icons.light_mode_outlined, action: () {}),
-          DisplayButton(iconData: Icons.backspace_outlined, action: () {}),
-        ],
-      ),
+      child:
+          BlocBuilder<AppThemeBloc, AppThemeState>(builder: (context, theme) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            DisplayButton(
+              iconData: theme.isLightTheme
+                  ? Icons.light_mode_outlined
+                  : Icons.dark_mode_outlined,
+              color: theme.theme.text,
+              action: () => appTheme.add(ToggleTheme()),
+            ),
+            DisplayButton(
+              iconData: Icons.backspace_outlined,
+              action: () {},
+              color: theme.isLightTheme ? Colors.red : Colors.red.shade300,
+            ),
+          ],
+        );
+      }),
     );
   }
 }
--- lib/widgets/display_button.dart
+++ lib/widgets/display_button.dart
@@ -1,13 +1,23 @@
+import 'package:calculator/states/app_theme/app_theme_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DisplayButton extends StatelessWidget {
-  DisplayButton({super.key, required this.iconData, required this.action});
+  DisplayButton({
+    super.key,
+    required this.iconData,
+    required this.action,
+    required this.color,
+  });
   late IconData iconData;
   late VoidCallback action;
+  late Color color;
   var shape = RoundedRectangleBorder(borderRadius: BorderRadius.circular(10));
 
   @override
   Widget build(BuildContext context) {
+    var theme = BlocProvider.of<AppThemeBloc>(context).state.theme;
+
     return SizedBox(
       height: 30,
       width: 30,
@@ -17,7 +27,16 @@ class DisplayButton extends StatelessWidget {
         child: InkWell(
           onTap: action,
           customBorder: shape,
-          child: Center(child: Icon(iconData, size: 16)),
+          hoverColor: theme.hover,
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          child: Center(
+            child: Icon(
+              iconData,
+              size: 16,
+              color: color,
+            ),
+          ),
         ),
       ),
     );
